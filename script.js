d7f41b04c138401ebcf4ae54a98371c59f8264f3
Rahma Mohamed Amer <102365623+Rahmaamer@users.noreply.github.com>
2023-11-14 14:55:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+ const inputBox = document.getElementById("input-box");
+const listContainer = document .getElementById("list-container");
+//if the user click add botton and there is no task 
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write any TASK!");
+
+    }
+    //the user add task 
+    else {
+        let li = document.createElement("li");
+        li.innerHTML =inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+
+    }
+    //clear the text box 
+    inputBox.value = "";
+    saveData();
+
+}
+//checked tasks
+listContainer.addEventListener("click",function(e){
+if(e.target.tagName === "LI"){
+    e.target.classList.toggle("checked");
+    saveData();
+
+
+}
+else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData();
+
+}
+
+},false);
+
+
+function saveData(){
+localStorage.setItem("data", listContainer.innerHTML);
+
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
+
+
